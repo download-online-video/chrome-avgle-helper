4ad12f7446a40bfcfaa0d2c2eef05710fceab594
vide0 <vide0-helper@gmx.com>
2019-01-30 16:27:15 +0800

    remove unused test

--- test/index.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-TEST_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
-pushd "$TEST_DIR" || exit 1;
-
-echo "[.] get_better_file_list";
-./get_better_file_list/test.sh || exit 1;
-
-echo "[+] all tests passed!";
