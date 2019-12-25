ae1936ae6472d4cf4cfaf7df0c53c2745878d954
vide0 <vide0-helper@gmx.com>
2019-12-25 17:02:31 +0800

    fix for travis

--- src/help/index.js
+++ src/help/index.js
@@ -3,48 +3,48 @@ import { ContextModules } from '../background/types';
 
 (function main() {
 	return;
-	const background = chrome.extension.getBackgroundPage();
-	const context = background && background.__avgle_helper_context;
-	if (!context) return;
+	// const background = chrome.extension.getBackgroundPage();
+	// const context = background && background.__avgle_helper_context;
+	// if (!context) return;
 
-	let animationTimer = 0;
+	// let animationTimer = 0;
 
-	/** @type {ContextModules} */
-	const modules = context.modules;
+	// /** @type {ContextModules} */
+	// const modules = context.modules;
 
-	modules.settings.storage.get().then(values => {
-		$$('[data-bind]').forEach(el => {
-			const bindName = el.getAttribute('data-bind');
-			const value = values[bindName];
-			if (typeof value !== 'undefined')
-				el.value = value;
-		});
-		$('#main').style.opacity = '1';
-	});
+	// modules.settings.storage.get().then(values => {
+	// 	$$('[data-bind]').forEach(el => {
+	// 		const bindName = el.getAttribute('data-bind');
+	// 		const value = values[bindName];
+	// 		if (typeof value !== 'undefined')
+	// 			el.value = value;
+	// 	});
+	// 	$('#main').style.opacity = '1';
+	// });
 
-	$('#btnSave').addEventListener('click', () => {
-		const values = {};
-		$$('[data-bind]').forEach(el => {
-			const bindName = el.getAttribute('data-bind');
-			const value = el.value;
-			if (typeof value !== 'undefined')
-				values[bindName] = value;
-		});
-		console.log(values);
-		modules.settings.storage.save(values).then(updated => {
-			showSavedNotification(null, updated);
-		})
-	});
+	// $('#btnSave').addEventListener('click', () => {
+	// 	const values = {};
+	// 	$$('[data-bind]').forEach(el => {
+	// 		const bindName = el.getAttribute('data-bind');
+	// 		const value = el.value;
+	// 		if (typeof value !== 'undefined')
+	// 			values[bindName] = value;
+	// 	});
+	// 	console.log(values);
+	// 	modules.settings.storage.save(values).then(updated => {
+	// 		showSavedNotification(null, updated);
+	// 	})
+	// });
 
-	function showSavedNotification(error, updated) {
-		clearTimeout(animationTimer);
-		const $notification = $('#notificationSave');
-		const style = $notification.style;
-		$notification.innerText = `Success: ${updated.length} settings!`;
+	// function showSavedNotification(error, updated) {
+	// 	clearTimeout(animationTimer);
+	// 	const $notification = $('#notificationSave');
+	// 	const style = $notification.style;
+	// 	$notification.innerText = `Success: ${updated.length} settings!`;
 
-		style.opacity = '1';
-		animationTimer = setTimeout(() => { style.opacity = '0'; }, 2000);
-	}
+	// 	style.opacity = '1';
+	// 	animationTimer = setTimeout(() => { style.opacity = '0'; }, 2000);
+	// }
 })();
 
 
